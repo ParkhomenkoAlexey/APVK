d2f1f7ecf85f6a452bc70ad0f68a9b99887a7ae2
Алексей Пархоменко <parkhomenko_ad@mail.ru>
2019-01-22 22:08:54 +0300

    Initial Commit

---
+++ VK Course/Scenes/FeedViewController/FeedViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FeedViewController.swift
+//  VK Course
+//
+//  Created by Алексей Пархоменко on 22/01/2019.
+//  Copyright © 2019 Алексей Пархоменко. All rights reserved.
+//
+
+import UIKit
+
+class FeedViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+}
+
